e17ce3646fa3ee9e9c2b8106c5d5e5db149c72b8
Tom Dale <thomas.dale2@gmail.com>
2022-04-21 09:31:28 -0700

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+//import {IxVideo} from '@imgix/shared-wc'
+import {IxVideo} from '@imgix/web-components'
+</script>
+
+<template>
+  <main>
+    <div
+    style="width: 500px; height: 500px; display: flex; justify-content: center; margin: auto"
+  >
+    <ix-video
+      width="600"
+      height="400"
+      controls
+      source="https://tomgcs.imgix.video/izzy_dance_party.mp4"
+    ></ix-video>
+  </div>
+  </main>
+</template>
