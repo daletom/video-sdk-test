5c30f053764af480ae404387cbc07346b74dfce7
Tom Dale <thomas.dale2@gmail.com>
2022-04-26 11:02:47 -0700

    add gallery

---
+++ src/views/GalleryView.vue
@@ -0,0 +1,33 @@
+<script setup>
+//import {IxVideo} from '@imgix/shared-wc'
+import {IxVideo} from '@imgix/web-components'
+</script>
+
+<template>
+  <main class="flex">
+    <ix-video
+      width="200px"
+      height="115px"
+      controls
+      source="https://tomgcs.imgix.video/izzy_dance_party.mp4"
+    ></ix-video>
+    <ix-video
+      controls
+      width="200px"
+      height="115px"
+      source="https://tomgcs.imgix.video/izzy_dance_party.mp4"
+    ></ix-video>
+    <ix-video
+      controls
+      width="200px"
+      height="115px"
+      source="https://tomgcs.imgix.video/izzy_dance_party.mp4"
+    ></ix-video>
+    <ix-video
+      controls
+      width="200px"
+      height="115px"
+      source="https://tomgcs.imgix.video/izzy_dance_party.mp4"
+    ></ix-video>
+  </main>
+</template>
